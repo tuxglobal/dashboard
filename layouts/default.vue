e9e5a25f6d16935eb336da433277f0e528b95b0c
Steve Harmeyer <harmeyersteve@gmail.com>
2023-02-15 21:35:52 -0700

    show connect button on all pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
             </div>
         </div>
         <div v-show="!loading && !connected" class="container py-5 d-grid gap-2">
-            <button v-show="!connected" @click="connect" type="button" class="btn btn-lg btn-info">Connect Wallet</button>
+            <button @click="connect" type="button" class="btn btn-lg btn-info">Connect Wallet</button>
         </div>
         <Footer />
     </div>
