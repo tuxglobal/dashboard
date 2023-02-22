36f5a826e75dbd00575dd3828aa75b91f44f39a1
Steve Harmeyer <harmeyersteve@gmail.com>
2023-02-21 19:23:17 -0700

    fixed case issue

--- components/usdcBlock.vue
+++ components/usdcBlock.vue
@@ -3,7 +3,7 @@
         <div class="card-body">
             <div class="card-text">
                 <div class="d-grid gap-2 mt-4">
-                    <button @click="mintUsdc" class="btn btn-lg btn-primary">Mint USDC</button>
+                    <button @click="mint" class="btn btn-lg btn-primary">Mint USDC</button>
                 </div>
             </div>
         </div>
